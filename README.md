95e34bff6702f341c4dd6f459bbf3c7915ae14e9
tedbauer <tjb272@cornell.edu>
2022-12-22 18:05:16 -0500

    Add hash-object

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ Git clone, following along with _[Write yourself a git!](https://wyag.thb.lt/#or
 
 ## Install
 ```sh
-cargo build && alias spit=./target/debug/spit
+cargo build && alias spit=$PWD/target/debug/spit
 ```
 
 ## Example usages
 ```sh
 spit init testrepo
+cd testrepo
+echo "test content" > file1.txt
+spit hash-object file1.txt
 ```
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use ini::Ini;
 use sha1::{Digest, Sha1};
 use std::ffi::OsStr;
 use std::fs;
+use std::io::Read;
 use std::path::PathBuf;
 
 struct GitRepository {
@@ -17,7 +18,9 @@ struct GitRepository {
 struct GitCommit {}
 struct GitTree {}
 struct GitTag {}
-struct GitBlob {}
+struct GitBlob {
+    content: String
+}
 
 enum GitObject {
     GitCommit(GitCommit),
@@ -27,18 +30,65 @@ enum GitObject {
 }
 
 impl GitObject {
-    fn serialize(data: &str) -> Self {
+    fn serialize(&self) -> String {
+        match self {
+            Self::GitBlob(b) => b.content.clone(),
+            _ => panic!("unimplemented")
+        }
+    }
+
+    fn deserialize(data: String) -> Self {
         todo!()
     }
 
-    fn deserialize(&self) -> String {
-        todo!()
+    fn type_header(&self) -> String {
+        match self {
+            Self::GitCommit(_) => "commit".to_string(),
+            Self::GitTree(_) => "tree".to_string(),
+            Self::GitTag(_) => "tag".to_string(),
+            Self::GitBlob(_) => "blob".to_string(),
+        }
     }
 }
 
 const DEFAULT_REPO_DESCRIPTION: &str =
     "Unnamed repository; edit this file 'description' to name the repository.\n";
 const DEFAULT_HEAD: &str = "ref: refs/heads/master\n";
+
+fn object_write(object: &GitObject) {
+    let object_data = object.serialize();
+    let object_data_with_header = format!(
+        "{} {}.{}",
+        object.type_header(),
+        object_data.len(),
+        object_data
+    );
+
+    let mut hasher = Sha1::new();
+    hasher.update(&object_data_with_header);
+    let sha = format!("{:x}", hasher.finalize());
+
+    let path = PathBuf::from(".")
+        .join(".git")
+        .join("objects")
+        .join(&sha[..2].to_string());
+
+    fs::create_dir_all(&path);
+    // TODO: content should be compressed
+    fs::write(path.join(&sha[2..]), object_data_with_header);
+}
+
+fn cmd_hash_object(path: &PathBuf, type_: &str) {
+    let file_data = fs::read_to_string(path).expect("couldn't open file");
+    let object = match type_ {
+        "commit" => GitObject::GitCommit(GitCommit {}),
+        "tree" => GitObject::GitTree(GitTree {}),
+        "tag" => GitObject::GitTag(GitTag {}),
+        "blob" => GitObject::GitBlob(GitBlob { content: file_data }),
+        _ => panic!("ugh handle this later"),
+    };
+    object_write(&object);
+}
 
 fn cmd_init(work_tree: &PathBuf) {
     if work_tree.exists() {
@@ -85,12 +135,36 @@ fn main() {
                         .index(1),
                 ),
         )
+        .subcommand(SubCommand::with_name("hash-object").args(&vec![
+            Arg::with_name("type"),
+            Arg::with_name("write"),
+            Arg::with_name("path"),
+        ]))
         .get_matches();
-    cmd_init(&PathBuf::from(
-        matches
-            .subcommand_matches("init")
-            .unwrap()
-            .value_of("dir")
-            .unwrap(),
-    ));
+
+    match matches.subcommand_name().as_deref() {
+        Some("init") => cmd_init(&PathBuf::from(
+            matches
+                .subcommand_matches("init")
+                .unwrap()
+                .value_of("dir")
+                .unwrap(),
+        )),
+        Some("hash-object") => cmd_hash_object(
+            &PathBuf::from(
+                matches
+                    .subcommand_matches("hash-object")
+                    .unwrap()
+                    .value_of("path")
+                    .unwrap(),
+            ),
+            matches
+                .subcommand_matches("hash-object")
+                .unwrap()
+                .value_of("type")
+                .unwrap(),
+        ),
+        Some(_) => println!("Command not recognized"),
+        None => println!("No command supplied."),
+    }
 }
